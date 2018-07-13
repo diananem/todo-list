df5bd9abbb4f993ee7a9d92108388e4a183d3489
diananem <diana.nemyrovska@gmail.com>
2018-07-13 20:10:11 +0300

    Added input

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  this.state = {
+    input: "",
+    tasks: []
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>TODO List:</p>
+        <input type="text" />
       </div>
     );
   }
