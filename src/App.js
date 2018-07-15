2b73639c5d9a37ea2804660ee404cbe11c6e50b7
diananem <diana.nemyrovska@gmail.com>
2018-07-15 18:49:53 +0300

    Adding new tasks

--- src/App.js
+++ src/App.js
@@ -3,15 +3,48 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  this.state = {
+  state = {
     input: "",
     tasks: []
-  }
+  };
+
+  toCreateTask = e => {
+    if (e.keyCode === 13) {
+      const tasks = this.state.tasks;
+      tasks.push({
+        done: false,
+        text: this.state.input
+      });
+      const newTask = this.setState({
+        tasks: tasks,
+        input: ""
+      });
+    }
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
   render() {
+    const isListNotEmpty = true;
+    console.log(this.state);
     return (
       <div className="App">
-        <p>TODO List:</p>
-        <input type="text" />
+        <h1 className="header-text">TODO List:</h1>
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          onChange={this.handleInputChange}
+          onKeyDown={this.toCreateTask}
+          value={this.state.input}
+        />
+        <ul>
+          {this.state.tasks.map((task, index) => (
+            <li key={index}>{task.text}</li>
+          ))}
+        </ul>
       </div>
     );
   }
