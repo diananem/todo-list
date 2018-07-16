8663853f985529299a5a4bf2c40800ea9d38b3b2
diananem <diana.nemyrovska@gmail.com>
2018-07-16 14:56:57 +0300

    Added toggle done/undone

--- src/App.js
+++ src/App.js
@@ -27,6 +27,40 @@ class App extends Component {
       input: e.target.value
     });
   };
+
+  toggleStatus = taskIndex => () => {
+    const updatedTasks = [...this.state.tasks];
+    updatedTasks[taskIndex] = {
+      done: !updatedTasks[taskIndex].done,
+      text: updatedTasks[taskIndex].text
+    };
+    this.setState({
+      tasks: updatedTasks
+    });
+  };
+  //1. Find task in array
+  //2. Change this task (change done to !this.state.tasks[taskIndex].done)
+  //3. Update state
+  renderTaskItems() {
+    return (
+      <ul className="todo-list">
+        {this.state.tasks.map((task, index) => (
+          <li key={index}>
+            <div className="list-item">
+              <input
+                className="checkbox-item"
+                type="checkbox"
+                id={index}
+                value={task.done}
+                onClick={this.toggleStatus(index)}
+              />
+              <label htmlFor={index}>{task.text}</label>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     const isListNotEmpty = true;
     console.log(this.state);
@@ -34,17 +68,15 @@ class App extends Component {
       <div className="App">
         <h1 className="header-text">TODO List:</h1>
         <input
+          className="main-input"
           type="text"
           placeholder="What needs to be done?"
           onChange={this.handleInputChange}
           onKeyDown={this.toCreateTask}
           value={this.state.input}
         />
-        <ul>
-          {this.state.tasks.map((task, index) => (
-            <li key={index}>{task.text}</li>
-          ))}
-        </ul>
+
+        {this.renderTaskItems()}
       </div>
     );
   }
